418eee9e45e78ba270a39f7cb40dcc48e762c41d
progr <progr@PROGRAMACAO109>
2023-11-08 10:06:35 -0300

    Add project files.

---
+++ LerCertificados/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LerCertificados
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			X509Store store = new X509Store(StoreLocation.CurrentUser);
+			store.Open(OpenFlags.OpenExistingOnly);
+
+			X509Certificate2Collection certCollection = store.Certificates;
+
+			foreach (X509Certificate2 c in certCollection)
+			{
+				try
+				{
+					Console.WriteLine($"Certificado: {c.Subject}");
+
+					var keyInfo = (c.PrivateKey as RSACryptoServiceProvider).CspKeyContainerInfo;
+
+					Console.WriteLine("Accessible property: " + keyInfo.Accessible);
+
+					Console.WriteLine("Exportable property: " + keyInfo.Exportable);
+
+					Console.WriteLine("HardwareDevice property: " + keyInfo.HardwareDevice);
+
+					Console.WriteLine("KeyContainerName property: " + keyInfo.KeyContainerName);
+
+					Console.WriteLine("KeyNumber property: " + keyInfo.KeyNumber);
+
+					Console.WriteLine("MachineKeyStore property: " + keyInfo.MachineKeyStore);
+
+					Console.WriteLine("Protected property: " + keyInfo.Protected);
+
+					Console.WriteLine("ProviderName property: " + keyInfo.ProviderName);
+
+					Console.WriteLine("ProviderType property: " + keyInfo.ProviderType);
+
+					Console.WriteLine("RandomlyGenerated property: " + keyInfo.RandomlyGenerated);
+
+					Console.WriteLine("Removable property: " + keyInfo.Removable);
+
+					Console.WriteLine("UniqueKeyContainerName property: " + keyInfo.UniqueKeyContainerName);
+				}
+				catch 
+				{
+					Console.WriteLine("Erro ao ler dados");
+				}
+				finally
+				{
+					Console.WriteLine("----------------------------------------------------------------------------\n");
+				}
+			}
+			store.Close();
+
+			Console.ReadLine();
+		}
+	}
+}
